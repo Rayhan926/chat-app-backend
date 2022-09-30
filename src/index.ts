df959e64fdfe08a6415ea72c71858f7727e67e6a
rayhan926 <rayhanbd7075@gmail.com>
2022-09-30 19:46:50 +0600

    Second try

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
+/* eslint-disable import/order */
+/* eslint-disable import/first */
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+require('dotenv').config();
+
+import axios from 'axios';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
+import http from 'http';
 import path from 'path';
+import { Server } from 'socket.io';
 import { PORT } from './config/contants';
-import { expressErrorMiddleware } from './utils';
+import connectDB from './db';
+import { expressErrorMiddleware, getApiFullPath } from './utils';
 
 // initialize app
 const app = express();
 
 // config dot env
-dotenv.config();
+// dotenv.config();
 
 // config cors
 app.use(cors());
@@ -18,19 +26,19 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // http server
-// const httpServer = http.createServer(app);
+const httpServer = http.createServer(app);
 
-// // init io
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: '*',
-//   },
-// });
+// init io
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
 
-// app.use((req: any, res, next) => {
-//   req.io = io;
-//   next();
-// });
+app.use((req: any, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.all('/', (req, res) => {
   res.send('Hello');
@@ -46,52 +54,52 @@ app.all('/', (req, res) => {
 // error middleware
 app.use(expressErrorMiddleware);
 
-// // socket on connection
-// io.on('connection', (socket) => {
-//   console.log(`New connection ${socket.id}`);
+// socket on connection
+io.on('connection', (socket) => {
+  console.log(`New connection ${socket.id}`);
 
-//   socket.on('join', (data) => {
-//     socket.join(data.id);
-//     axios
-//       .post(
-//         getApiFullPath('/user/update-socket-id'),
-//         {
-//           socketId: socket.id,
-//         },
-//         {
-//           headers: {
-//             authorization: data.token,
-//           },
-//         }
-//       )
-//       // .then((res) => console.log(res.data))
-//       .catch(console.log);
-//   });
+  socket.on('join', (data) => {
+    socket.join(data.id);
+    axios
+      .post(
+        getApiFullPath('/user/update-socket-id'),
+        {
+          socketId: socket.id,
+        },
+        {
+          headers: {
+            authorization: data.token,
+          },
+        }
+      )
+      // .then((res) => console.log(res.data))
+      .catch(console.log);
+  });
 
-//   socket.on('saw-message', (data) => {
-//     socket.to(data?.sawUserId).emit('seen-messages', {
-//       seenBy: data?.sawBy,
-//     });
-//   });
+  socket.on('saw-message', (data) => {
+    socket.to(data?.sawUserId).emit('seen-messages', {
+      seenBy: data?.sawBy,
+    });
+  });
 
-//   socket.on('typing', (data) => {
-//     socket.to(data?.to).emit('typing', {
-//       typingStatus: data?.typingStatus,
-//       from: data?.from,
-//     });
-//   });
+  socket.on('typing', (data) => {
+    socket.to(data?.to).emit('typing', {
+      typingStatus: data?.typingStatus,
+      from: data?.from,
+    });
+  });
 
-//   socket.on('disconnect', () => {
-//     axios
-//       .post(getApiFullPath('/user/remove-socket-id'), {
-//         socketId: socket.id,
-//       })
-//       // .then((res) => console.log(res.data))
-//       .catch(console.log);
-//   });
-// });
+  socket.on('disconnect', () => {
+    axios
+      .post(getApiFullPath('/user/remove-socket-id'), {
+        socketId: socket.id,
+      })
+      // .then((res) => console.log(res.data))
+      .catch(console.log);
+  });
+});
 
-// connectDB().then(() => {
-//   console.log('Database connection successfull');
-// });
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+connectDB().then(() => {
+  console.log('Database connection successfull');
+});
+httpServer.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
