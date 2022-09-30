d93eab2cc5a9e33b0009450a7dbd6e9801ab47bf
rayhan926 <rayhanbd7075@gmail.com>
2022-09-30 21:12:31 +0600

    ss

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -1,10 +1,20 @@
 import { Request } from 'express';
+import fs from 'fs';
 import createHttpError from 'http-errors';
 import multer from 'multer';
 import path from 'path';
 
+const uploadFolderPath = './public';
 const storage = multer.diskStorage({
-  destination: (req: Request, file: any, cb: any) => cb(null, './public/uploads'),
+  destination: (req: Request, file: any, cb: any) => {
+    console.log('path', fs.existsSync(uploadFolderPath));
+    if (!fs.existsSync(uploadFolderPath)) {
+      fs.mkdirSync(path.join(uploadFolderPath));
+      cb(null, uploadFolderPath);
+    } else {
+      cb(null, uploadFolderPath);
+    }
+  },
   filename: (req: Request, file: any, cb: any) => {
     const fileExt = path.extname(file.originalname);
     const fileName = `${file.originalname
