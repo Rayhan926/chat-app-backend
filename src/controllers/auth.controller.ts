27b99ad575bdc8b19aae67643d05135a79d07be8
rayhan926 <rayhanbd7075@gmail.com>
2022-09-30 20:16:50 +0600

    ss

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,55 +1,57 @@
+import axios from 'axios';
 import { NextFunction, Request, Response } from 'express';
+import User from '../models/user.model';
 import { createResponse } from '../utils';
 
 export const googleLogin = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { tokenId } = req.body;
 
-    // // get user data from access token
-    // const { data } = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
-    //   headers: {
-    //     Authorization: tokenId,
-    //   },
-    // });
+    // get user data from access token
+    const { data } = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+      headers: {
+        Authorization: tokenId,
+      },
+    });
 
-    // // check if user exist
-    // const existingUser: any = await User.findOne({ email: data.email }).select(
-    //   '-friends -sentFriendRequests -friendRequests'
-    // );
+    // check if user exist
+    const existingUser: any = await User.findOne({ email: data.email }).select(
+      '-friends -sentFriendRequests -friendRequests'
+    );
 
-    // if (existingUser) {
-    //   const token = await existingUser.generateToken();
+    if (existingUser) {
+      const token = await existingUser.generateToken();
 
-    //   res.send(
-    //     createResponse({
-    //       message: 'Login success',
-    //       data: {
-    //         token,
-    //         user: existingUser,
-    //       },
-    //     })
-    //   );
-    // } else {
-    //   // create user
-    //   const user = new User({
-    //     name: data.name,
-    //     email: data.email,
-    //     avatar: data.picture,
-    //     username: data.email.split('@')[0],
-    //   });
+      res.send(
+        createResponse({
+          message: 'Login success',
+          data: {
+            token,
+            user: existingUser,
+          },
+        })
+      );
+    } else {
+      // create user
+      const user = new User({
+        name: data.name,
+        email: data.email,
+        avatar: data.picture,
+        username: data.email.split('@')[0],
+      });
 
-    //   await user.save();
-    //   const token = await (user as any).generateToken();
-    //   res.send(
-    //     createResponse({
-    //       data: {
-    //         token,
-    //         user,
-    //       },
-    //       message: 'Login success',
-    //     })
-    //   );
-    // }
+      await user.save();
+      const token = await (user as any).generateToken();
+      res.send(
+        createResponse({
+          data: {
+            token,
+            user,
+          },
+          message: 'Login success',
+        })
+      );
+    }
 
     res.send(
       createResponse({
